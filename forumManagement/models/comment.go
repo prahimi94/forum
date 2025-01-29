06edb9a34785851c176a0c943359c33273378bfe
Mohammad Mahdi Kheirkhah <mahdikheirkhah060@gmail.com>
2025-01-29 16:00:11 +0200

    adding model for comment

---
+++ forumManagement/models/comment.go
@@ -0,0 +1,488 @@
+package models
+
+import (
+	userManagementModels "forum/userManagement/models"
+	"forum/utils"
+	"log"
+	"time"
+)
+
+type Comment struct {
+	ID               int                       `json:"id"`
+	PostId           int                       `json:"post_id"`
+	Description      string                    `json:"description"`
+	UserId           int                       `json:"user_id"`
+	Status           string                    `json:"status"`
+	CreatedAt        time.Time                 `json:"created_at"`
+	UpdatedAt        *time.Time                `json:"updated_at"`
+	UpdatedBy        *int                      `json:"updated_by"`
+	IsLikedByUser    bool                      `json:"liked"`
+	IsDislikedByUser bool                      `json:"disliked"`
+	NumberOfLikes    int                       `json:"number_of_likes"`
+	NumberOfDislikes int                       `json:"number_of_dislikes"`
+	Post             Post                      `json:"post"`
+	User             userManagementModels.User `json:"user"`
+}
+
+func InsertComment(postId int, userId int, description string) (int, error) {
+	db := utils.OpenDBConnection()
+	defer db.Close() // Close the connection after the function finishes
+
+	// Start a transaction for atomicity
+	tx, err := db.Begin()
+	if err != nil {
+		return -1, err
+	}
+	insertQuery := `INSERT INTO comments (post_id, description, user_id) VALUES (?, ?, ?);`
+	result, insertErr := tx.Exec(insertQuery, postId, description, userId)
+	if insertErr != nil {
+		tx.Rollback() // Rollback on error
+		// Check if the error is a SQLite constraint violation
+		return -1, insertErr
+	}
+	// Retrieve the last inserted ID
+	lastInsertID, errFind := result.LastInsertId()
+	if errFind != nil {
+		tx.Rollback() // Rollback on error
+		log.Fatal(errFind)
+		return -1, errFind
+	}
+	return int(lastInsertID), nil
+}
+
+func UpdateComment(comment *Comment, user_id int, newDescription string) error {
+	db := utils.OpenDBConnection()
+	defer db.Close() // Close the connection after the function finishes
+
+	// Start a transaction for atomicity
+	updateQuery := `UPDATE comments
+					SET description = ?,
+						updated_at = CURRENT_TIMESTAMP,
+						updated_by = ?
+					WHERE id = ?;`
+	_, updateErr := db.Exec(updateQuery, newDescription, user_id, comment.ID)
+	if updateErr != nil {
+		return updateErr
+	}
+
+	return nil
+}
+
+func UpdateCommentStatus(comment *Comment, status string, user_id int) error {
+	db := utils.OpenDBConnection()
+	defer db.Close() // Close the connection after the function finishes
+
+	updateQuery := `UPDATE posts
+					SET status = ?,
+						updated_at = CURRENT_TIMESTAMP,
+						updated_by = ?
+					WHERE id = ?;`
+	_, updateErr := db.Exec(updateQuery, status, user_id, comment.ID)
+	if updateErr != nil {
+		return updateErr
+	}
+
+	return nil
+}
+
+func ReadAllComments() ([]Comment, error) {
+	db := utils.OpenDBConnection()
+	defer db.Close() // Close the connection after the function finishes
+
+	var comments []Comment
+	selectQuery := `
+		SELECT 
+			p.id AS post_id, p.uuid AS post_uuid, p.title AS post_title, p.description AS post_description, 
+			p.status AS post_status, p.created_at AS post_created_at, p.updated_at AS post_updated_at, p.updated_by AS post_updated_by,
+			c.id AS comment_id, c.user_id AS comment_user_id, c.description AS comment_description, 
+			c.status AS comment_status, c.created_at AS comment_created_at, c.updated_at AS comment_updated_at, c.updated_by AS comment_updated_by,
+			u.id AS user_id, u.uuid AS user_uuid, u.username AS user_username, u.name AS user_name, u.type AS user_type, u.email AS user_email,  
+			u.status AS user_status, u.created_at AS user_created_at, u.updated_at AS user_updated_at, u.updated_by AS user_updated_by,
+		FROM comments c
+		INNER JOIN posts p ON c.post_id = p.id AND p.status != 'delete' AND c.status != 'delete'
+		INNER JOIN users u ON c.user_id = u.id AND u.status != 'delete'
+	`
+	// Query the records
+	rows, selectError := db.Query(selectQuery)
+
+	if selectError != nil {
+		return nil, selectError
+	}
+	defer rows.Close() // Ensure rows are closed after processing
+
+	// Iterate over rows and populate the slice
+	for rows.Next() {
+		var comment Comment
+		var user userManagementModels.User
+		var post Post
+		err := rows.Scan(
+			&post.ID,
+			&post.UUID,
+			&post.Title,
+			&post.Description,
+			&post.Status,
+			&post.CreatedAt,
+			&post.UpdatedAt,
+			&post.UpdatedBy,
+
+			&user.ID,
+			&user.UUID,
+			&user.Username,
+			&user.Name,
+			&user.Type,
+			&user.Email,
+			&user.Status,
+			&user.CreatedAt,
+			&user.UpdatedAt,
+			&user.UpdatedBy,
+
+			&comment.ID,
+			&comment.PostId,
+			&comment.Description,
+			&comment.UserId,
+			&comment.Status,
+			&comment.CreatedAt,
+			&comment.UpdatedAt,
+			&comment.UpdatedBy,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+		comment.Post = post
+		comment.User = user
+
+		comments = append(comments, comment)
+	}
+
+	// Check for any errors during the iteration
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return comments, nil
+}
+
+func ReadCommentsFromUserId(userId int) ([]Comment, error) {
+	db := utils.OpenDBConnection()
+	defer db.Close() // Close the connection after the function finishes
+
+	var comments []Comment
+
+	// Updated query to join comments with posts
+	selectQuery := `
+		SELECT 
+			p.id AS post_id, p.uuid AS post_uuid, p.title AS post_title, p.description AS post_description, 
+			p.status AS post_status, p.created_at AS post_created_at, p.updated_at AS post_updated_at, p.updated_by AS post_updated_by,
+			c.id AS comment_id, c.user_id AS comment_user_id, c.description AS comment_description, 
+			c.status AS comment_status, c.created_at AS comment_created_at, c.updated_at AS comment_updated_at, c.updated_by AS comment_updated_by
+		FROM comments c
+		INNER JOIN posts p ON c.post_id = p.i
+		WHERE c.status != 'delete' AND p.status != 'delete' AND c.user_id = ?;
+	`
+
+	rows, selectError := db.Query(selectQuery, userId) // Query the database
+	if selectError != nil {
+		return nil, selectError
+	}
+	defer rows.Close() // Ensure rows are closed after processing
+
+	// Iterate over rows and populate the slice
+	for rows.Next() {
+		var comment Comment
+		var post Post
+
+		err := rows.Scan(
+			// Map post fields
+			&post.ID,
+			&post.UUID,
+			&post.Title,
+			&post.Description,
+			&post.Status,
+			&post.CreatedAt,
+			&post.UpdatedAt,
+			&post.UpdatedBy,
+
+			// Map comment fields
+			&comment.ID,
+			&comment.UserId,
+			&comment.Description,
+			&comment.Status,
+			&comment.CreatedAt,
+			&comment.UpdatedAt,
+			&comment.UpdatedBy,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		// Assign the post to the comment
+		comment.Post = post
+
+		comments = append(comments, comment)
+	}
+
+	// Check for any errors during the iteration
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return comments, nil
+}
+
+func ReadAllCommentsForPost(postId int, userID int) ([]Comment, error) {
+	db := utils.OpenDBConnection()
+	defer db.Close() // Close the connection after the function finishes
+
+	var comments []Comment
+	commentMap := make(map[int]*Comment)
+	// Updated query to join comments with posts
+	selectQuery := `
+		SELECT 
+			u.id AS user_id, u.uuid AS user_uuid, u.username AS user_username, u.name AS user_name, u.type AS user_type, u.email AS user_email,  
+			u.status AS user_status, u.created_at AS user_created_at, u.updated_at AS user_updated_at, u.updated_by AS user_updated_by,
+			c.id AS comment_id, c.user_id AS comment_user_id, c.description AS comment_description, 
+			c.status AS comment_status, c.created_at AS comment_created_at, c.updated_at AS comment_updated_at, c.updated_by AS comment_updated_by,
+			cl.type
+		FROM comments c
+			INNER JOIN users u
+				ON c.user_id = u.id AND c.status != 'delete' AND u.status != 'delete' AND c.post_id = ?
+			INNER JOIN comment_likes cl
+				ON c.id = cl.comment_id AND cl.status != 'delete';
+	`
+	rows, selectError := db.Query(selectQuery, postId) // Query the database
+	if selectError != nil {
+		return nil, selectError
+	}
+	defer rows.Close() // Ensure rows are closed after processing
+
+	// Iterate over rows and populate the slice
+	for rows.Next() {
+		var comment Comment
+		var user userManagementModels.User
+		var Type string
+		err := rows.Scan(
+			// Map post fields
+			&user.ID,
+			&user.UUID,
+			&user.Username,
+			&user.Name,
+			&user.Type,
+			&user.Email,
+			&user.Status,
+			&user.CreatedAt,
+			&user.UpdatedAt,
+			&user.UpdatedBy,
+
+			// Map comment fields
+			&comment.ID,
+			&comment.UserId,
+			&comment.Description,
+			&comment.Status,
+			&comment.CreatedAt,
+			&comment.UpdatedAt,
+			&comment.UpdatedBy,
+
+			&Type,
+		)
+		if existingComment, found := commentMap[comment.ID]; found {
+			if user.ID == userID {
+				if Type == "like" {
+					existingComment.IsLikedByUser = true
+				} else if Type == "dislike" {
+					existingComment.IsDislikedByUser = true
+				}
+			}
+			if Type == "like" {
+				existingComment.NumberOfLikes++
+			} else if Type == "dislike" {
+				existingComment.NumberOfDislikes++
+			}
+		} else {
+			if user.ID == userID {
+				if Type == "like" {
+					comment.IsLikedByUser = true
+				} else if Type == "dislike" {
+					comment.IsDislikedByUser = true
+				}
+			}
+			if Type == "like" {
+				comment.NumberOfLikes++
+			} else if Type == "dislike" {
+				comment.NumberOfDislikes++
+			}
+
+			commentMap[comment.ID] = &comment
+		}
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Check for any errors during the iteration
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	// Convert the map of comments into a slice
+	for _, comment := range commentMap {
+		comments = append(comments, *comment)
+	}
+
+	return comments, nil
+}
+
+// func ReadAllCommentsForPostLikedByUser(postId int, userId int) ([]Comment, error) {
+// 	db := utils.OpenDBConnection()
+// 	defer db.Close() // Close the connection after the function finishes
+
+// 	var comments []Comment
+
+// 	// Updated query to join comments with posts
+// 	selectQuery := `
+// 		SELECT
+// 			u.id AS user_id, u.uuid AS user_uuid, u.username AS user_username, u.name AS user_name, u.type AS user_type, u.email AS user_email,
+// 			u.status AS user_status, u.created_at AS user_created_at, u.updated_at AS user_updated_at, u.updated_by AS user_updated_by,
+// 			c.id AS comment_id, c.user_id AS comment_user_id, c.description AS comment_description,
+// 			c.status AS comment_status, c.created_at AS comment_created_at, c.updated_at AS comment_updated_at, c.updated_by AS comment_updated_by,
+// 			count(CASE WHEN cl.type = 'like' THEN 1 END) as likes_count, count(CASE WHEN cl.type = 'dislike' THEN 1 END) as dislikes_count
+// 		FROM comments c
+// 			INNER JOIN users u
+// 				ON c.user_id = u.id AND c.status != 'delete' AND u.status != 'delete' AND c.post_id = ?
+// 			INNER JOIN comment_likes cl
+// 				ON c.id = cl.comment_id AND cl.status != 'delete'
+// 		GROUP BY cl.comment_id;
+// 	`
+// 	rows, selectError := db.Query(selectQuery, postId) // Query the database
+// 	if selectError != nil {
+// 		return nil, selectError
+// 	}
+// 	defer rows.Close() // Ensure rows are closed after processing
+
+// 	// Iterate over rows and populate the slice
+// 	for rows.Next() {
+// 		var comment Comment
+// 		var user userManagementModels.User
+
+// 		err := rows.Scan(
+// 			// Map post fields
+// 			&user.ID,
+// 			&user.UUID,
+// 			&user.Username,
+// 			&user.Name,
+// 			&user.Type,
+// 			&user.Email,
+// 			&user.Status,
+// 			&user.CreatedAt,
+// 			&user.UpdatedAt,
+// 			&user.UpdatedBy,
+
+// 			// Map comment fields
+// 			&comment.ID,
+// 			&comment.UserId,
+// 			&comment.Description,
+// 			&comment.Status,
+// 			&comment.CreatedAt,
+// 			&comment.UpdatedAt,
+// 			&comment.UpdatedBy,
+// 			&comment.NumberOfLikes,
+// 			&comment.NumberOfDislikes,
+// 		)
+
+// 		if err != nil {
+// 			return nil, err
+// 		}
+
+// 		// Assign the post to the comment
+// 		comment.User = user
+
+// 		comments = append(comments, comment)
+// 	}
+
+// 	// Check for any errors during the iteration
+// 	if err := rows.Err(); err != nil {
+// 		return nil, err
+// 	}
+
+// 	return comments, nil
+// }
+
+func ReadAllCommentsOfUserForPost(postId int, userId int) ([]Comment, error) {
+	db := utils.OpenDBConnection()
+	defer db.Close() // Close the connection after the function finishes
+
+	var comments []Comment
+	selectQuery := `
+		SELECT 
+			p.id AS post_id, p.uuid AS post_uuid, p.title AS post_title, p.description AS post_description, 
+			p.status AS post_status, p.created_at AS post_created_at, p.updated_at AS post_updated_at, p.updated_by AS post_updated_by,
+			c.id AS comment_id, c.user_id AS comment_user_id, c.description AS comment_description, 
+			c.status AS comment_status, c.created_at AS comment_created_at, c.updated_at AS comment_updated_at, c.updated_by AS comment_updated_by,
+			u.id AS user_id, u.uuid AS user_uuid, u.username AS user_username, u.name AS user_name, u.type AS user_type, u.email AS user_email,  
+			u.status AS user_status, u.created_at AS user_created_at, u.updated_at AS user_updated_at, u.updated_by AS user_updated_by,
+		FROM comments c
+		INNER JOIN posts p ON c.post_id = p.id AND p.status != 'delete' AND c.status != 'delete' AND p.id = ?
+		INNER JOIN users u ON c.user_id = u.id AND u.status != 'delete'
+		where u.id = ?
+	`
+	// Query the records
+	rows, selectError := db.Query(selectQuery, postId, userId)
+
+	if selectError != nil {
+		return nil, selectError
+	}
+	defer rows.Close() // Ensure rows are closed after processing
+
+	// Iterate over rows and populate the slice
+	for rows.Next() {
+		var comment Comment
+		var user userManagementModels.User
+		var post Post
+		err := rows.Scan(
+			&post.ID,
+			&post.UUID,
+			&post.Title,
+			&post.Description,
+			&post.Status,
+			&post.CreatedAt,
+			&post.UpdatedAt,
+			&post.UpdatedBy,
+
+			&user.ID,
+			&user.UUID,
+			&user.Username,
+			&user.Name,
+			&user.Type,
+			&user.Email,
+			&user.Status,
+			&user.CreatedAt,
+			&user.UpdatedAt,
+			&user.UpdatedBy,
+
+			&comment.ID,
+			&comment.PostId,
+			&comment.Description,
+			&comment.UserId,
+			&comment.Status,
+			&comment.CreatedAt,
+			&comment.UpdatedAt,
+			&comment.UpdatedBy,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+		comment.Post = post
+		comment.User = user
+
+		comments = append(comments, comment)
+	}
+
+	// Check for any errors during the iteration
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return comments, nil
+}
