37f84ccd07354fc156a43ef7c4384e074098b060
p-rahimi <parisa_rahimy@yahoo.com>
2025-01-31 11:51:06 +0200

    commit before push

--- forumManagement/controllers/postController.go
+++ forumManagement/controllers/postController.go
@@ -255,3 +255,134 @@ func SubmitPost(w http.ResponseWriter, r *http.Request) {
 
 	http.Redirect(w, r, "/", http.StatusFound)
 }
+
+func EditPost(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		errorManagementControllers.HandleErrorPage(w, r, errorManagementControllers.MethodNotAllowedError)
+		return
+	}
+
+	loginStatus, loginUser, _, checkLoginError := userManagementControllers.CheckLogin(r)
+	if checkLoginError != nil {
+		errorManagementControllers.HandleErrorPage(w, r, errorManagementControllers.InternalServerError)
+		return
+	}
+	if loginStatus {
+		fmt.Println("logged in userid is: ", loginUser.ID)
+		// return
+	} else {
+		fmt.Println("user is not logged in")
+	}
+
+	uuid, errUrl := utils.ExtractUUIDFromUrl(r.URL.Path, "post")
+	if errUrl == "not found" {
+		errorManagementControllers.HandleErrorPage(w, r, errorManagementControllers.NotFoundError)
+		return
+	}
+
+	post, err := models.ReadPostByUUID(uuid)
+	if err != nil {
+		errorManagementControllers.HandleErrorPage(w, r, errorManagementControllers.InternalServerError)
+		return
+	}
+
+	categories, err := models.ReadAllCategories()
+	if err != nil {
+		errorManagementControllers.HandleErrorPage(w, r, errorManagementControllers.InternalServerError)
+		return
+	}
+
+	data_obj_sender := struct {
+		LoginUser  userManagementModels.User
+		Post       models.Post
+		Categories []models.Category
+	}{
+		LoginUser:  loginUser,
+		Post:       post,
+		Categories: categories,
+	}
+
+	tmpl, err := template.ParseFiles(
+		publicUrl+"edit_post.html",
+		publicUrl+"templates/header.html",
+		publicUrl+"templates/navbar.html",
+		publicUrl+"templates/footer.html",
+	)
+	if err != nil {
+		errorManagementControllers.HandleErrorPage(w, r, errorManagementControllers.InternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, data_obj_sender)
+	if err != nil {
+		errorManagementControllers.HandleErrorPage(w, r, errorManagementControllers.InternalServerError)
+		return
+	}
+}
+
+func UpdatePost(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPut {
+		errorManagementControllers.HandleErrorPage(w, r, errorManagementControllers.MethodNotAllowedError)
+		return
+	}
+
+	loginStatus, loginUser, _, checkLoginError := userManagementControllers.CheckLogin(r)
+	if checkLoginError != nil {
+		errorManagementControllers.HandleErrorPage(w, r, errorManagementControllers.InternalServerError)
+		return
+	}
+	if loginStatus {
+		fmt.Println("logged in userid is: ", loginUser.ID)
+		// return
+	} else {
+		fmt.Println("user is not logged in")
+	}
+
+	err := r.ParseForm()
+	if err != nil {
+		errorManagementControllers.HandleErrorPage(w, r, errorManagementControllers.BadRequestError)
+		return
+	}
+
+	title := r.FormValue("title")
+	description := r.FormValue("description")
+	categories := r.Form["categories"]
+	if len(title) == 0 || len(description) == 0 || len(categories) == 0 {
+		errorManagementControllers.HandleErrorPage(w, r, errorManagementControllers.BadRequestError)
+		return
+	}
+
+	post := &models.Post{
+		Title:       title,
+		Description: description,
+		UserId:      loginUser.ID,
+	}
+
+	// Convert the string slice to an int slice
+	categoryIds := make([]int, 0, len(categories))
+	for _, category := range categories {
+		if id, err := strconv.Atoi(category); err == nil {
+			categoryIds = append(categoryIds, id)
+		} else {
+			// Handle error if conversion fails (for example, invalid input)
+			http.Error(w, "Invalid category ID", http.StatusBadRequest)
+			return
+		}
+	}
+
+	// Update a record while checking duplicates
+	updateError := models.UpdatePost(post, categoryIds, loginUser.ID)
+	if updateError != nil {
+		if errors.Is(updateError, sql.ErrNoRows) {
+			// todo show toast
+			fmt.Println("Post already exists!")
+		} else {
+			errorManagementControllers.HandleErrorPage(w, r, errorManagementControllers.InternalServerError)
+		}
+		return
+	} else {
+		fmt.Println("Post updated successfully!")
+	}
+
+	userManagementControllers.RedirectToPrevPage(w, r)
+}
